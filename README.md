6629f31d4fff43a1767caa1a0ea11bb74432e732
Sanya Alabai <70968297+sanyaalabai@users.noreply.github.com>
2025-06-29 20:06:38 +0300

    Time-related functions

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # cool-lib
 Cool and Simple Web Library for (mostly) styling
+
+## Contents
+- cool.css  
+    Helpful styling one-liners. Useful if you don't use tailwindcss or anything like that.
+- cool.js  
+    Some simple but useful functions (delay, copy to clipboard, etc.)
+- header.js  
+    Authomatic sticky header.
+
+## TODO
+- Themeing script
+- Basic color styles
+- Wiki-esque stylesheet with theme support
+- Interactions with URL
+- Add more TODOs
--- cool.js
+++ cool.js
@@ -1,13 +1,25 @@
 const copyTextToClipboard = async (text) => {
-        try {
-          await navigator.clipboard.writeText(text);
-
-          console.log("Text copied to clipboard successfully!");
-        } catch (error) {
-          console.error("Failed to copy text:", error);
-          window.prompt(
-            "To copy text press: Ctrl+C, Enter",
-            text
-          );
-        }
-      };
+  try {
+    await navigator.clipboard.writeText(text);
+    console.log("Text copied to clipboard successfully!");
+  } catch (error) {
+    console.error("Failed to copy text:", error);
+    window.prompt(
+      "To copy text press: Ctrl+C, Enter",
+      text
+    );
+  }
+};
+function seconds(t) {return t*1000;}
+function minutes(t) {return t*60*1000;}
+function hours(t) {return t*3600*1000;}
+///Requires time in milliseconds (use 'seconds(t)', 'minutes(t)', etc.).
+///Also required to be awaited.
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+///Requires time in milliseconds (use 'seconds(t)', 'minutes(t)', etc.).
+async function executeDelayed(func, ms) {
+  await delay(ms);
+  func();
+}
