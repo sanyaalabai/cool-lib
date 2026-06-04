3a959902e1464d7c91d4c3ae87e3412adcddcdfd
sanya.alabai <70968297+sanyaalabai@users.noreply.github.com>
2026-06-04 20:36:58 +0300

    Removed duplicates

--- cool.js
+++ cool.js
@@ -51,12 +51,6 @@ export function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min)) + min;
 }
-export function getRandomInt(max) {
-  return getRandomInt(0,max);
-}
 export function getRandomFloat(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
-}
-export function getRandomFloat(max) {
-  return getRandomFloat(0,max);
 }
